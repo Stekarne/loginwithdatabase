4eb21feaf96df0ef9cfe0d74c9125fac135fcd1f
Aysen Furhoff <aysen.furhoff@botkyrka.se>
2018-02-13 11:12:20 +0100

    Adminform uppdaterad

--- Loginwithdatabase/Forms/Adminform.Designer.cs
+++ Loginwithdatabase/Forms/Adminform.Designer.cs
@@ -33,73 +33,118 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_Insert = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Uppdate = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 70);
+            this.label1.Location = new System.Drawing.Point(173, 81);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.Size = new System.Drawing.Size(112, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Användarnamn: ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(81, 141);
+            this.label2.Location = new System.Drawing.Point(173, 167);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.Size = new System.Drawing.Size(76, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Lösenord: ";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(156, 67);
+            this.textBox1.Location = new System.Drawing.Point(324, 81);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(156, 138);
+            this.textBox2.Location = new System.Drawing.Point(324, 153);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
             this.textBox2.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(67, 9);
+            this.label3.Location = new System.Drawing.Point(278, 22);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(232, 13);
+            this.label3.Size = new System.Drawing.Size(202, 17);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Här kan du som administratör skapa nya konton";
+            this.label3.Text = "Skapa uppdatera radera konto";
             // 
-            // button1
+            // btn_Insert
             // 
-            this.button1.Location = new System.Drawing.Point(132, 179);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Skapa";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btn_Insert.Location = new System.Drawing.Point(176, 220);
+            this.btn_Insert.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Insert.Name = "btn_Insert";
+            this.btn_Insert.Size = new System.Drawing.Size(100, 28);
+            this.btn_Insert.TabIndex = 5;
+            this.btn_Insert.Text = "Skapa";
+            this.btn_Insert.UseVisualStyleBackColor = true;
+            this.btn_Insert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Location = new System.Drawing.Point(457, 222);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(99, 26);
+            this.btn_Delete.TabIndex = 6;
+            this.btn_Delete.Text = "Radera";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_Uppdate
+            // 
+            this.btn_Uppdate.Location = new System.Drawing.Point(324, 220);
+            this.btn_Uppdate.Name = "btn_Uppdate";
+            this.btn_Uppdate.Size = new System.Drawing.Size(99, 28);
+            this.btn_Uppdate.TabIndex = 7;
+            this.btn_Uppdate.Text = "Uppdatera";
+            this.btn_Uppdate.UseVisualStyleBackColor = true;
+            this.btn_Uppdate.Click += new System.EventHandler(this.btn_Uppdate_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(176, 311);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(380, 150);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Adminform
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(363, 230);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(818, 455);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_Uppdate);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Insert);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Adminform";
             this.Text = "Adminform";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,6 +157,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_Insert;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Uppdate;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Loginwithdatabase/Forms/Adminform.cs
+++ Loginwithdatabase/Forms/Adminform.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,16 +13,100 @@ namespace Loginwithdatabase.Forms
 {
     public partial class Adminform : Form
     {
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Sample;Integrated Security=true;");
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
+        //ID variable used in Updating and Deleting Record  
+        int Password = 0;
         public Adminform()
         {
             InitializeComponent();
+            DisplayData();
         }
         Classes.adminlogin adminlogin = new Classes.adminlogin();
         private void button1_Click(object sender, EventArgs e)
         {
-            adminlogin.user = textBox1.Text;
-            adminlogin.pass = textBox2.Text;
-            adminlogin.login();
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                cmd = new SqlCommand("insert into Login(Name,Password) values(@name,@passowrd)", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@name", textBox1.Text);
+                cmd.Parameters.AddWithValue("@password", textBox2.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Saved Successfully");
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Provide Details!");
+            }
+        }
+        //Display Data in DataGridView  
+        private void DisplayData()
+        {
+            con.Open();
+            DataTable dt = new DataTable();
+            adapt = new SqlDataAdapter("select * from Login", con);
+            adapt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        //Clear Data  
+        private void ClearData()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            Password = 0;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Password = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+        }
+
+        private void btn_Uppdate_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                cmd = new SqlCommand("update Login set Name=@name where Password=@password", con);
+                con.Open();
+                //cmd.Parameters.AddWithValue("@id", ID);
+                cmd.Parameters.AddWithValue("@name", textBox1.Text);
+                cmd.Parameters.AddWithValue("@password", textBox2.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Updated Successfully");
+                con.Close();
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Row to Update");
+            }
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            if (Password != 0)
+            {
+                cmd = new SqlCommand("delete Login where Password=@password", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@password", Password);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Row Deleted Successfully!");
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Row to Delete");
+            }
         }
     }
 }
